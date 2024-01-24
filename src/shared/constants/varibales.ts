627d3aea75e700a53264e40ae1a7c468763f9497
DESKTOP-57ET6VJ\박동진 <djpark@sphinfo.co.kr>
2024-01-24 17:15:18 +0900

    test2 (+1 squashed commits)
    Squashed commits:
    
    [ab02bfc] test

--- src/shared/constants/varibales.ts
+++ src/shared/constants/varibales.ts
@@ -8,3 +8,5 @@ export const PipelineLayerIdsInGeoserver = [
 export const GeoserverWMSRequestURL = 'http://34.64.161.119:8080/geoserver/geolab/wms';
 export const GeolabVectorTileStyle = 'http://34.64.161.119:8081/data/pipeline_samplesv2.json';
 export const CircleSize = 3;
+const message = 'test';
+console.log(message)
