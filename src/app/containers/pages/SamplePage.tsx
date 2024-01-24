65c800400330032f66a0ee680c5b7701635f36ac
BoraJeong <brjeong@sphinfo.co.kr>
2024-01-24 16:38:10 +0900

    [UI] 스타일 변경

--- src/app/containers/pages/SamplePage.tsx
+++ src/app/containers/pages/SamplePage.tsx
@@ -45,21 +45,6 @@ const AsideHeader = styled(Link)`
     line-height: normal;
   }
 `;
-/*
-export const Header = styled.header`
-  background-color: var(--white-a100);
-  height: 5.625rem;
-  box-shadow: 0 0 4px 0 rgba(0, 0, 0, 0.1);
-  grid-area: header;
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  padding: 0.625rem 1.25rem;
-  em {
-    font-size: 1.5rem;
-    margin-right: 0.88rem;
-  }
-`; */
 
 const Main = styled.main`
   background-color: var(--light-surface-level-2);
